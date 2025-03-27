94b0c7381330656dab709ecd0f152577430978d0
Andrew Broberg <andrew@broberg.id.au>
2025-03-28 10:06:44 +1100

    use onenord theme for nvim and ghostty

---
+++ nvim/lua/plugins/onenord.lua
@@ -0,0 +1,7 @@
+return {
+    'rmehri01/onenord.nvim',
+    config = function()
+        require('onenord').setup()
+        vim.cmd.colorscheme 'onenord'
+    end
+}
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,47 +1,7 @@
 return {
     {
-        "olimorris/onedarkpro.nvim",
-        priority = 1000,
-    },
-    {
-        "nickkadutskyi/jb.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        "sonph/onehalf",
-        lazy = false,
-        priority = 1000,
-        config = function(plugin)
-            vim.opt.rtp:append(plugin.dir .. "/vim")
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("catppuccin")
-        end,
     },
-    {
-        'projekt0n/github-nvim-theme',
-        name = 'github-theme',
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require('github-theme').setup({
-                options = {
-                    transparent = false, -- Enable transparency
-                }
-            })
-            -- vim.cmd('colorscheme github_light')
-        end,
-    }
 }
