f13e8e09a3ff50affb459a27b89b85d5d06b54e8
Andrew Broberg <andrew@broberg.id.au>
2026-09-15 09:54:07 +1000

    feat(nvim): add jsonl syntax highlighting and linting

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -3,12 +3,25 @@ return {
   config = function()
     local lint = require('lint')
 
+    lint.linters.jsonl = {
+      cmd = 'jq',
+      args = { 'empty' },
+      stdin = true,
+      stream = 'stderr',
+      ignore_exitcode = true,
+      parser = require('lint.parser').from_pattern(
+        '(.+) at line (%d+), column (%d+)',
+        { 'message', 'lnum', 'col' }
+      ),
+    }
+
     lint.linters_by_ft = {
       javascript = { 'eslint' },
       vue = { 'eslint' },
       javascriptreact = { 'eslint' },
       typescript = { 'eslint' },
-      typescriptreact = { 'eslint' }
+      typescriptreact = { 'eslint' },
+      jsonl = { 'jsonl' },
     }
 
     -- Configure eslint_d to use flat config (eslint.config.js)
